80dd7ea8ac901a681b4425e23cc3bd8501ad451e
Emmanuel <georgexx009@hotmail.com>
2023-09-11 18:36:22 -0600

    encode query parameters

--- basenode/basenode.go
+++ basenode/basenode.go
@@ -1,1 +1,29 @@
 package basenode
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func MakeHTTPRequest[T any](fullUrl string, httpMethod string, headers map[string]string, queryParameters url.Values, body io.Reader, responseType T) (T, error) {
+	client := http.Client{}
+
+	// check if the url is valid
+	url, err := url.Parse(fullUrl)
+	if err != nil {
+		return responseType, err
+	}
+
+	// if the method is GET, lets append the query parameters
+	if httpMethod == "GET" {
+		q := url.Query()
+
+		for k, v := range queryParameters {
+			q.Set(k, strings.Join(v, ","))
+		}
+
+		url.RawQuery = q.Encode()
+	}
+}
